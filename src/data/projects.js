c37fc9c4a165a75aebaf1679420124d32440b089
Puru-Ram <163015777+Puru-Ram@users.noreply.github.com>
2024-11-16 22:27:55 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,32 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Cloud-Native System for Scalable File Processing',
-    subtitle: 'A GCP-powered microservice architecture for real-time data management.',
-    date: '2024-10-28',
+    title: 'Rally Car Design Challenge',
+    subtitle: 'Represented a college "Team GARUDA" in the national level championship and stood 7th place out of 52 teams.',
+    date: '2017-02-01',
     desc:
-      'Designed a cloud-native system on GCP with Cloud Run, Pub/Sub, and Firestore to ensure scalability and real-time data processing. '
-      + 'Backend services in NodeJs were containerized with Docker and deployed on Cloud Run, with React handling the frontend. '
-      + 'Implemented role-based access control (RBAC) and fault-tolerant design for security, seamless communication, and workload management.',
+      'Monitored and managed a group of 28 students for the design and fabrication of an 800cc, 2-seater utility track vehicle specially suitable for desert terrain, for an All India level utility terrain vehicle design and fabrication competition organized by Polaris Motorsports, India.', 
+'The final three-day event, consisting of Technical Inspection, hill Climb, Suspension, and traction tests, and 4-hour endurance race held at the Thar Desert, in the month of February-2018',
   },
   {
-    title: 'Campus Event Management System',
-    subtitle: 'An automated solution for managing college events and boosting student participation.',
+    title: 'IMPROVING AVAILABILITY THROUGH ROOT CAUSE ANALYSIS OF DIESEL GENERATORS',
+    subtitle: 'Carried out a “mini-project” as a partial fulfillment of academic curriculum for Pre-Final Year.',
     desc:
-      'Developed to streamline the organization of events within a college campus. '
-      + 'This full-stack application is powered by Java Spring Boot for the backend, with RESTful APIs managing event registrations, '
-      + 'attendance tracking, and notifications. React serves as the frontend,'
-      + 'Implemented as part of a college project, the platform successfully facilitated multiple events, increasing participation by 30% within the first semester.',
+      'Performed root cause analysis (RCA) to identify recurring problems.',
+    'Assessed current maintenance practices and identified gaps.',
+    'Recommended optimized maintenance schedules and solutions.',
+    'Proposed actionable measures to reduce downtime and improve reliability.',
+    'Significance: Contributed to better performance and efficiency of diesel generators.',
+  },
+  {
+    title: 'IMPROVEMENT OF 6S CONDITIONS IN MIX MODEL ASSEMBLY LINE, BY EFFICIENT SPACE MANAGEMENT(USING DMAIC METHODOLOGY)',
+    subtitle: 'Carried out a project at Volvo Construction Equipment Ltd. (VCE), Peenya, Bangalore as a partial fulfillment of academic curriculum for Final Year.',
+    desc:
+      'Applied DMAIC (Define, Measure, Analyze, Improve, Control) methodology to identify inefficiencies in space utilization.',
+    'Conducted a detailed analysis of space allocation and workflow.',
+    'Developed optimized layouts for enhanced productivity and safety.',
+    'Outcome: Improved 6S compliance by streamlining material flow and maximizing available space.',
+    'Significance: Contributed to improved operational efficiency and a safer working environment.',
   },
 ];
 
